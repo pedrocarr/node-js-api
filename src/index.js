2199eee524b84285e5bfb7988f5d258ab0386100
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-30 13:51:22 -0300

    feat: add routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello World!!"});
-})
+app.get('/courses', (req, res) => {
+    return res.json(["Curso1", "Curso2", "Curso3"]);
+});
+
+app.post('/courses', (req, res) => {
+    return res.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
+});
+
+app.put('/courses/:id', (req, res) => {
+    return res.json(["Curso 6", "Curso2", "Curso3", "Curso4"]);
+});
+
+app.patch('/courses/:id', (req, res) => {
+    return res.json(["Curso 6", "Curso2", "Curso3", "Curso4"]);
+});
+
 
 app.listen(3333);
