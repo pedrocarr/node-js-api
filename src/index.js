4bbecce6b7593e87fe935c0b6b8fa079c321cbf4
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-30 14:35:12 -0300

    docs: update Readme

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json())
 
 app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query)
+
     return res.json(["Curso1", "Curso2", "Curso3"]);
 });
 
 app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
     return res.json(["Curso1", "Curso2", "Curso3", "Curso4"]);
 });
 
 app.put('/courses/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
     return res.json(["Curso 6", "Curso2", "Curso3", "Curso4"]);
 });
 
