e77f9c154a776a59534e426f8bda13db40f49aca
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-30 20:53:24 -0300

    feat: implement middleware

--- src/index.js
+++ src/index.js
@@ -13,6 +13,20 @@ const customers = [];
  * statement []
  */
 
+function verifyIfExistsAccountCPF(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find((customer) => customer.cpf === cpf);
+
+    if(!customer) {
+        return res.status(400).json({ error: "Customer not found!" });
+    };
+
+    req.customer = customer;
+
+    return next();
+}
+
 
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body;
@@ -34,17 +48,10 @@ app.post('/account', (req, res) => {
     return res.status(201).send();
 });
 
+// app.use(verifyIfExistsAccountCPF);
 
-app.get('/statement/', (req, res) => {
-
-    const { cpf } = req.headers;
-
-    const customer = customers.find((customer) => customer.cpf === cpf);
-
-    if(!customer) {
-        return res.status(400).json({ error: "Customer not found!" });
-    };
-
+app.get('/statement/', verifyIfExistsAccountCPF, (req, res) => {
+    const { customer } = req;
     return res.json(customer.statement);
     
 
