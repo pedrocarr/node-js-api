952eaf3a3a77c8b5d016a5aaec3f6605288f39a9
Pedro Carvalho <eng.pedro.amb@gmail.com>
2021-11-30 16:39:27 -0300

    chore: verify if cpf already exists

--- src/index.js
+++ src/index.js
@@ -14,10 +14,14 @@ const customers = [];
  */
 
 
-app.post('/account', (req, resp) => {
+app.post('/account', (req, res) => {
     const { cpf, name } = req.body;
 
-    
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf);
+
+    if(customerAlreadyExists) {
+        return res.status(400).json({ error: "Customer Already Exists!" });
+    }
     const id = uuidv4();
 
     customers.push({
